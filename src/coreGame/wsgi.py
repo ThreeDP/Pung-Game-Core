5fe6f3945603c3a297738c9a3ebd1eee3b8343a4
Davy Paulino da Silva Dantas <CJ300225X@ifspcjoad.ifspcjo.edu.br>
2024-09-28 11:11:54 -0300

    feat: add a start from game

--- src/coreGame/wsgi.py
+++ src/coreGame/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import asyncio
+import websockets
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coreGame.settings')
 
 application = get_wsgi_application()
+
+async def echo(websocket, path):
+    async for message in websocket:
+        print(f"Recebido: {message}")
+        # Aqui você pode usar os modelos do Django
+        articles = Article.objects.all()  # Exemplo de consulta
+        await websocket.send(f"Você disse: {message}. Total de artigos: {articles.count()}")
+
+start_server = websockets.serve(echo, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+print("Servidor WebSocket está rodando na porta 8765...")
+asyncio.get_event_loop().run_forever()
