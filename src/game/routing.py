d46605598d1edaa41b8289abc1246fe9d3d677d6
Davy Paulino <37334813+ThreeDP@users.noreply.github.com>
2024-11-12 02:58:12 +0000

    feat: game logic

--- src/game/routing.py
+++ src/game/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .consumers import http_consumer
 
 websocket_urlpatterns = [
-    path('ws/game/', http_consumer.as_asgi()),
+    path('api/v1/game-core/', http_consumer.as_asgi()),
 ]
 
